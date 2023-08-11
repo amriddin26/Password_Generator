f584af3ed3cdaf652c6e9965b975df335cd3d3c0
amriddin06 <amriddinjorayev7@gmail.com>
2023-08-11 12:15:12 +0300

    Passwor_Generatord

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let password = document.querySelector('#button');
+
 function genPassword(){
-    let chars = "0123456789abcdefgxijkmlopqrstuvhyshchomnzwu!@#$%^$*()QWERTYUIOPASDFGHJKLZXCVBNM";
+    let chars = '0123456789abcdefgxijkmlopqrstuvhyshchomnzwu!@#$%^$*()QWERTYUIOPASDFGHJKLZXCVBNM';
     let passwordLength = 12;
     let password = '';
-    for (let i = 0; i < password; i++){
+    for (let i = 0; i < passwordLength; i++){
         let randomNuber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNuber, randomNuber + 1);
     }
@@ -14,6 +15,6 @@ function genPassword(){
 function copyPassword() {
     let copyText = document.querySelector('#password');
     copyText.select();
-    copyText.setSelctRange(0,999);
+    copyText.setSelectionRange(0, 999);
     document.execCommand('copy');
 }
